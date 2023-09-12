00bed878e94d8e70a51f82ff1f88ddcb397eeee3
Msarazua <sarazuamaria@gmail.com>
2023-09-12 16:32:57 -0600

    Se agregan librerías.

--- resources/views/marcas/index.blade.php
+++ resources/views/marcas/index.blade.php
@@ -1,4 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <form method="POST" action="{{ route('marcas.store') }}" enctype="multipart/form-data"
     onsubmit="return checkSubmit();">
@@ -30,6 +31,18 @@
             <tr>
                 <th scope="row">{{ $index->id }}</th>
                 <td>{{ $index->nombre }}</td>
+                <td class="center">
+                    <div style="margin-top:5px;margin-bottom:5px">
+                        <a href="{{ url('marcas/' . $index->id . '/edit') }}" class="btn btn btn-primary"
+                            title="Editar">
+                            <i class="fas fa-edit icon-action-btn"></i>
+                        </a>
+                        <a class="btn btn btn-danger" title="Eliminar"
+                            onclick="event.preventDefault(); document.getElementById('formDel{{ $index->id }}').submit();">
+                            <i class="fas fa-trash-alt icon-action-btn2"></i>
+                        </a>
+                    </div> 
+                </td>
             </tr>
             @endforeach
         </tbody>
