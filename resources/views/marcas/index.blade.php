6cfca1f0ecdc2c3e4b64dc2584bd7abb7677afa3
Msarazua <sarazuamaria@gmail.com>
2023-09-13 22:35:14 -0600

    Template de proyecto

--- resources/views/marcas/index.blade.php
+++ resources/views/marcas/index.blade.php
@@ -1,5 +1,5 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+@extends('layout.index')
+@section('content')
 
     <form method="POST" action="{{ route('marcas.store') }}" enctype="multipart/form-data"
     onsubmit="return checkSubmit();">
@@ -26,54 +26,93 @@
             </div>
         </div>
     </form>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Estado</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($marcas as $index)
-            <tr>
-                <th scope="row">{{ $index->id }}</th>
-                <td>{{ $index->nombre }}</td>
-                @if ($index->estado == 1)
-                    <td>
-                        <form method="POST"
-                        action="{{ url('marcas/' . $index->id) }}">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="_method" value="PUT">
-                            <button title="Presione para desactivar" class="btn btn-success">
-                                Activa
-                            </button>
-                        </form>
-                    </td>
-                @else
-                    <td>
-                        <form method="POST"
-                        action="{{ url('modificar/marcas/' . $index->id) }}">
-                            {{ csrf_field() }}
-                            <button title="Presione para activar" class="btn btn-danger">
-                                Inactiva
-                            </button>
-                        </form>
-                    </td>
-                @endif
-                {{-- <td class="center">
-                    <div style="margin-top:5px;margin-bottom:5px">
-                        <a href="{{ url('marcas/' . $index->id . '/edit') }}" class="btn btn btn-primary"
-                            title="Editar">
-                            <i class="fas fa-edit icon-action-btn"></i>
-                        </a>
-                        <a class="btn btn btn-danger" title="Desactivar marca"
-                            onclick="event.preventDefault(); document.getElementById('formDel{{ $index->id }}').submit();">
-                            <i class="fas fa-trash-alt icon-action-btn2"></i>
-                        </a>
-                    </div> 
-                </td> --}}
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>Dashboard</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li><a href="#">Dashboard</a></li>
+                            <li><a href="#">Table</a></li>
+                            <li class="active">Data table</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Data Table</strong>
+                        </div>
+                        <div class="card-body">
+                            <table id="bootstrap-data-table-export" class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Estado</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($marcas as $index)
+                                    <tr>
+                                        <th scope="row">{{ $index->id }}</th>
+                                        <td>{{ $index->nombre }}</td>
+                                        @if ($index->estado == 1)
+                                            <td>
+                                                <form method="POST"
+                                                action="{{ url('marcas/' . $index->id) }}">
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="_method" value="PUT">
+                                                    <button title="Presione para desactivar" class="btn btn-success">
+                                                        Activa
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        @else
+                                            <td>
+                                                <form method="POST"
+                                                action="{{ url('modificar/marcas/' . $index->id) }}">
+                                                    {{ csrf_field() }}
+                                                    <button title="Presione para activar" class="btn btn-danger">
+                                                        Inactiva
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        @endif
+                                        {{-- <td class="center">
+                                            <div style="margin-top:5px;margin-bottom:5px">
+                                                <a href="{{ url('marcas/' . $index->id . '/edit') }}" class="btn btn btn-primary"
+                                                    title="Editar">
+                                                    <i class="fas fa-edit icon-action-btn"></i>
+                                                </a>
+                                                <a class="btn btn btn-danger" title="Desactivar marca"
+                                                    onclick="event.preventDefault(); document.getElementById('formDel{{ $index->id }}').submit();">
+                                                    <i class="fas fa-trash-alt icon-action-btn2"></i>
+                                                </a>
+                                            </div> 
+                                        </td> --}}
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
+@endsection
