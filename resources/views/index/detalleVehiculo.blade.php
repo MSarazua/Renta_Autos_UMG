86f5865d9bccb0d681dd19ce34e3c9d821b9048d
Luis Velasquez <139912145+Marianovh@users.noreply.github.com>
2023-10-26 03:09:20 -0600

    detalle vehiculo responsive

--- resources/views/index/detalleVehiculo.blade.php
+++ resources/views/index/detalleVehiculo.blade.php
@@ -1,32 +1,40 @@
 @extends('layout.index')
 @section('content')
-
     <div class="container">
         <div class="row">
             @foreach ($detalleAutos->Message as $index)
                 @if ($index->ID_Vehiculo == $idVehiculo)
                     <div class="row">
-                        <div class="col">
+                        <div class="col-md-12">
                             <img class="homeCar" src= "{{ $index->ImgCar }}">
                         </div>
-                        <div class="container">
-                            <div class="row">
-                                @foreach ($marcas->Message as $indexM)
-                                    @if ($indexM->ID_Marca == $index->Marca)
-                                        <div class="col-md-9 titulo_vehiculo" style="text-align: end">{{ $indexM->Marca }} {{ $index->Linea }}</div>
-                                        <div class="col-md-3"><button type="button" class="btn border-0" data-bs-toggle="modal" data-bs-target="#exampleModal_{{$index->ID_Vehiculo}}">
+
+                        <div class="row ">
+                            @foreach ($marcas->Message as $indexM)
+                                @if ($indexM->ID_Marca == $index->Marca)
+                                    <div class="col-md-9 titulo_vehiculo" style="text-align: end">{{ $indexM->Marca }}
+                                        {{ $index->Linea }}</div>
+                                    <div class="col-md-3">
+                                        <button type="button" class="btn border-0" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal_{{ $index->ID_Vehiculo }}">
                                             <img style="width: 20%" src="{{ asset('img/reservar.png') }}">
-                                        </button></div>
-                                        <!-- Modal -->
-                                        <div class="modal fade" id="exampleModal_{{$index->ID_Vehiculo}}" tabindex="-1" aria-labelledby="exampleModalLabel_{{$index->ID_Vehiculo}}" aria-hidden="true">
-                                            <div class="modal-dialog">
+                                        </button>
+                                    </div>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal_{{ $index->ID_Vehiculo }}" tabindex="-1"
+                                        aria-labelledby="exampleModalLabel_{{ $index->ID_Vehiculo }}" aria-hidden="true">
+                                        <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header" style="border-bottom: 3px solid black !important">
-                                                    <h5 class="modal-title" style="text-align: center !important" id="exampleModalLabel_{{$index->ID_Vehiculo}}"><b>RESERVA DE VEHÍCULO</b></h5>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    <h5 class="modal-title" style="text-align: center !important"
+                                                        id="exampleModalLabel_{{ $index->ID_Vehiculo }}">
+                                                        <b>RESERVA DE VEHÍCULO</b>
+                                                    </h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
-                                                    @if ($index->Disponible == "Si")
+                                                    @if ($index->Disponible == 'Si')
                                                         <p>
                                                             <b>Vehículo disponible</b>
                                                         </p>
@@ -36,67 +44,90 @@
                                                         <input type="hidden" name="_method" value="POST">
                                                         <input type="hidden" name="ID_Reserva" value="6">
                                                         <input type="hidden" name="ID_DetalleReserva" value="6">
-                                                        <input type="hidden" name="ID_Usuario" value="{{ $id_usuario }}">
-                                                        <input type="hidden" name="ID_Vehiculo" value="{{$index->ID_Vehiculo}}">
+                                                        <input type="hidden" name="ID_Usuario"
+                                                            value="{{ $id_usuario }}">
+                                                        <input type="hidden" name="ID_Vehiculo"
+                                                            value="{{ $index->ID_Vehiculo }}">
                                                         <div class="input-group mb-3">
-                                                            <span class="input-group-text" id="basic-addon1">Fecha de salida</span>
-                                                            <input type="date" class="form-control" name="FechaSalida" placeholder="Fecha de salida" aria-label="Fecha de salida">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                Fecha de salida
+                                                            </span>
+                                                            <input type="date" class="form-control" name="FechaSalida"
+                                                                placeholder="Fecha de salida" aria-label="Fecha de salida">
                                                         </div>
                                                         <div class="input-group mb-3">
-                                                            <span class="input-group-text" id="basic-addon1">Fecha de entrada</span>
-                                                            <input type="date" class="form-control" name="FechaEntrada" placeholder="Fecha de entrada" aria-label="Fecha de entrada">
+                                                            <span class="input-group-text" id="basic-addon1">
+                                                                Fecha de entrada
+                                                            </span>
+                                                            <input type="date" class="form-control" name="FechaEntrada"
+                                                                placeholder="Fecha de entrada"
+                                                                aria-label="Fecha de entrada">
                                                         </div>
                                                         <div class="input-group mb-3">
                                                             <span class="input-group-text">Q</span>
-                                                            <input type="text" class="form-control" name="PrecioBase" placeholder="Precio base" aria-label="Precio base">
+                                                            <input type="text" class="form-control" name="PrecioBase"
+                                                                placeholder="Precio base" aria-label="Precio base">
                                                         </div>
                                                         <div class="input-group mb-3">
                                                             <span class="input-group-text">%</span>
-                                                            <input type="text" class="form-control" name="Descuento" placeholder="Descuento" aria-label="Descuento">
+                                                            <input type="text" class="form-control" name="Descuento"
+                                                                placeholder="Descuento" aria-label="Descuento">
                                                         </div>
                                                         <div class="input-group mb-3">
                                                             <span class="input-group-text">+</span>
-                                                            <input type="text" class="form-control" name="Adicionales" placeholder="Adicionales" aria-label="Adicionales">
+                                                            <input type="text" class="form-control" name="Adicionales"
+                                                                placeholder="Adicionales" aria-label="Adicionales">
                                                         </div>
                                                         <div class="input-group mb-3">
                                                             <span class="input-group-text">Q</span>
-                                                            <input type="text" class="form-control" name="PrecioFinalCliente" placeholder="Precio Final" aria-label="Precio Final">
+                                                            <input type="text" class="form-control"
+                                                                name="PrecioFinalCliente" placeholder="Precio Final"
+                                                                aria-label="Precio Final">
                                                         </div>
                                                         <div class="input-group mb-3">
-                                                            <input type="text" class="form-control" name="PrecioTotal" placeholder="Precio Total" aria-label="Precio Total">
+                                                            <input type="text" class="form-control" name="PrecioTotal"
+                                                                placeholder="Precio Total" aria-label="Precio Total">
                                                         </div>
                                                         <div class="input-group mb-3">
-                                                            <input type="text" class="form-control" name="Descripcion" placeholder="Descripción" aria-label="Descripcion">
+                                                            <input type="text" class="form-control" name="Descripcion"
+                                                                placeholder="Descripción" aria-label="Descripcion">
                                                         </div>
-                                                        <button type="submit" class="btn btn-lg text-light" style="background-color: #3C486B; float: right; border-radius: 20px">
+                                                        <button type="submit" class="btn btn-lg text-light"
+                                                            style="background-color: #3C486B; float: right; border-radius: 20px">
                                                             Confirmar reserva
                                                         </button>
                                                     </form>
                                                 </div>
                                             </div>
-                                            </div>
                                         </div>
-                                    @endif
-                                @endforeach
-                            </div>
-                            <div class="row" style="margin-top: 6%">
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+
+                        <div class="container" style="margin-top: 8rem">
+                            <div class="row">
                                 <div class="col">
                                     <div class="circle-container ">
                                         <div class="circle circle-image img1">
                                         </div>
                                         <div class="circle circle-text">
-                                            <p>Transmisión</p>
-                                            <p>{{ $index->Transmision }}</p>
+                                            <div class="boxt">
+                                                <p>Transmisión</p>
+                                                <p>{{ $index->Transmision }}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col">
+                                <div class="col mx-auto">
                                     <div class="circle-container">
                                         <div class="circle circle-image img2">
                                         </div>
                                         <div class="circle circle-text">
-                                            <p>Año</p>
-                                            <p>{{ $index->Ano }}</p>
+                                            <div class="boxt">
+                                                <p>Año</p>
+                                                <p>{{ $index->Ano }}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -105,8 +136,11 @@
                                         <div class="circle circle-image img3">
                                         </div>
                                         <div class="circle circle-text">
-                                            <p>Asientos</p>
-                                            <p>{{ $index->Asientos }}</p>
+                                            <div class="boxt">
+
+                                                <p>Asientos</p>
+                                                <p>{{ $index->Asientos }}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -115,17 +149,19 @@
                                         <div class="circle circle-image img4">
                                         </div>
                                         <div class="circle circle-text">
-                                            <p>Motor</p>
-                                            <p>{{ $index->Motor }}</p>
+                                            <div class="boxt">
+
+                                                <p>Motor</p>
+                                                <p>{{ $index->Motor }}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 @endif
             @endforeach
         </div>
     </div>
-
 @endsection
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -3,71 +3,81 @@
 <html class="no-js" lang="en">
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Sintony:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Sintony:wght@700&display=swap"
+        rel="stylesheet">
     <script src="https://kit.fontawesome.com/8cb75b282f.js" crossorigin="anonymous"></script>
 </head>
 <style>
-      .bg_header {
-          background-color: #C70039 !important;
-          font-size: 1.5rem;
-      }
-      /* Añade una regla para los elementos específicos que no deseas afectar (como formularios) */
-      .content {
-          position: relative;
-          padding: 20px;
-      }
+    .bg_header {
+        background-color: #C70039 !important;
+        font-size: 1.5rem;
+    }
 
-      .img_login {
+    /* Añade una regla para los elementos específicos que no deseas afectar (como formularios) */
+    .content {
+        position: relative;
+        padding: 20px;
+    }
+
+    .img_login {
         width: 2.5rem;
-      }
+    }
 
-      /* Añade una regla para los elementos específicos que no deseas afectar (como formularios) */
-      * {
-          position: relative;
-          font-family: 'Roboto Mono', monospace !important;
-          font-family: 'Sintony', sans-serif !important;
-      }
+    /* Añade una regla para los elementos específicos que no deseas afectar (como formularios) */
+    * {
+        position: relative;
+        font-family: 'Roboto Mono', monospace !important;
+        font-family: 'Sintony', sans-serif !important;
+    }
 
-      .titulo_vehiculo {
+    .titulo_vehiculo {
         color: black;
         font-size: 3rem;
         text-transform: uppercase;
         font-weight: 700
-      }
+    }
 
-      .btn_reservar {
-        border-radius: 50px; 
+    .btn_reservar {
+        border-radius: 50px;
         background-color: #47B5FF;
         font-size: 2rem
-      }
-      .homeCar{
+    }
+
+    .homeCar {
         width: 85%;
-      }
-      #carouselExampleDark>*{
+    }
+
+    #carouselExampleDark>* {
         z-index: 3;
-      }
-      
-      .card-group-bg-white {
-          background-color: #fff; /* Color blanco */
-          padding: 20px; /* Agrega espacio de relleno si es necesario */
-      }
-      .btn_vehiculo {
-        border-radius: 50px; 
+    }
+
+    .card-group-bg-white {
+        background-color: #fff;
+        /* Color blanco */
+        padding: 20px;
+        /* Agrega espacio de relleno si es necesario */
+    }
+
+    .btn_vehiculo {
+        border-radius: 50px;
         background-color: #C70039;
         font-size: 1.5rem;
         min-width: 50%;
         max-width: 100%;
-      }
+    }
 
-      .card_vehiculos {
+    .card_vehiculos {
         margin-left: 3% !important
-      }
+    }
 
-      .circle-container {
+    .circle-container {
         border: solid 2px #fff;
         width: 15vw;
         /* Ajusta el ancho según tus necesidades */
@@ -140,40 +150,141 @@
 
     }
 
-    .circle-text>p {
+
+    .boxt>p {
         z-index: 5;
-        /* Capa inferior */
         text-align: center;
-        line-height: 100px;
-        /* Centrar verticalmente el texto */
-        color: #fff;
-        /* Color del texto */
+        line-height: 100%;
     }
 
+    .boxt {
+        color: #fff;
+        margin-top: 8rem;
+        text-align: center;
+        font-size: 1.5rem;
+    }
+
+    @media (max-width: 1101px) {
+        .circle-container {
+            border: solid 2px #fff;
+            width: 5vw;
+            /* Ajusta el ancho según tus necesidades */
+            position: relative;
+            height: 15vh;
+            /* Ajusta la altura según tus necesidades */
+            margin: 3rem;
+            margin-bottom: 5rem;
+        }
+
+        .circle {
+            border: solid 2px #C70039;
+            width: 130px;
+            /* Diámetro del círculo */
+            height: 130px;
+            /* Diámetro del círculo */
+            border-radius: 15%;
+            border-top-left-radius: 45%;
+            border-top-right-radius: 45%;
+            /* Forma de círculo */
+            position: absolute;
+            top: 30%;
+            left: 30%;
+            transform: translate(-50%, -50%);
+            /* Centrar el círculo */
+        }
+
+        .circle-image,
+        .img1,
+        .img2,
+        .img3,
+        .img4 {
+
+            /* Color del círcurgb(52, 152, 219)imagen */
+            z-index: 3;
+            /* Capa superior */
+            margin-top: -5rem;
+            border-radius: 100%;
+            /* URL de tu imagen */
+
+            /* Ajusta el tamaño de la imagen */
+        }
+
+        .img1 {
+            background-image: url('https://media-public.canva.com/MADGybaN6W0/4/screen.jpg');
+            background-size: cover;
+            background-position: center
+        }
+
+        .img2 {
+            background-image: url('https://media-public.canva.com/MADGvhL0euM/7/screen.jpg');
+            background-size: cover;
+            background-position: center
+        }
+
+        .img3 {
+            background-image: url('https://media-public.canva.com/MADGvugSTuo/7/screen.jpg');
+            background-size: cover;
+            background-position: center
+        }
+
+        .img4 {
+            background-image: url('https://media-public.canva.com/MADGyUsMJOE/4/screen.jpg');
+            background-size: cover;
+            background-position: center
+        }
+
+        .circle-text {
+            background-color: #C70039;
+            /* Color del círculo del texto */
+            z-index: 1;
+
+        }
+
+
+        .boxt>p {
+            z-index: 5;
+            text-align: center;
+            line-height: 100%;
+        }
+
+        .boxt {
+            color: #fff;
+            margin-top: 3.5rem;
+            text-align: center;
+            font-size: 1rem;
+        }
+    }
 </style>
+
 <body class="bg_body">
     <header class="p-3 bg-dark text-white bg_header">
         <div class="container">
-          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-            </a>
-    
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" class="nav-link px-2 text-white">Inicio</a></li>
-              <li><a href="#" class="nav-link px-2 text-white">Reservar</a></li>
-              <li><a href="#" class="nav-link px-2 text-white">Cotizar</a></li>
-            </ul>
-            <div class="text-end">
-              <button type="button" class="btn me-2"><img class="img_login" src= "{{ asset('img/usuario.png') }}"></button>
-              {{-- <button type="button" class="btn btn-warning">Sign-up</button> --}}
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-white">Inicio</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Reservar</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Cotizar</a></li>
+                </ul>
+                <div class="text-end">
+                    <button type="button" class="btn me-2"><img class="img_login"
+                            src= "{{ asset('img/usuario.png') }}"></button>
+                    {{-- <button type="button" class="btn btn-warning">Sign-up</button> --}}
+                </div>
             </div>
-          </div>
         </div>
-      </header>
+    </header>
     <div class="content mt-3">
         @yield('content')
     </div> <!-- .content -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
